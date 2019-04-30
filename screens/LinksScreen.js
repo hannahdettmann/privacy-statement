a01d6bbd7797f8617e7d50bfec6d657d8d3a57a1
dettma1 <32919333+dettma1@users.noreply.github.com>
2019-04-30 15:54:06 -0500

    updated Link Screen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Input, Text } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
-//import { withStyles } from '@material-ui/core/styles';
-//import Slider from '@material-ui/lab/Slider';
+
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
@@ -13,8 +12,9 @@ export default class LinksScreen extends React.Component {
     return (
       <ScrollView style={styles.container}>
         <Text style={styles.getStartedText}>
-              Welcome to Safe+Driving! Here, you can choose your level 
+              Welcome to Safe+Driving! Here, you can choose your level of saftey. 
             </Text>
+
       </ScrollView>
     );
   }
@@ -23,6 +23,7 @@ export default class LinksScreen extends React.Component {
  _takeInput = () => {
     return 0;
   };
+
 
 const styles = StyleSheet.create({
   container: {
